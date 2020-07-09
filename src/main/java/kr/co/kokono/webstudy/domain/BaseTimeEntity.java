55e2b56c3ca2354fbbdb94d68124bdf9ec8b6295
soongu <naong526@naver.com>
2020-07-09 18:22:47 +0900

    2020.07.09 Posts업데이트 더티체킹기능, 생성수정시간 자동화기능

---
+++ src/main/java/kr/co/kokono/webstudy/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package kr.co.kokono.webstudy.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass //If entity classes inherit this class, make the fields recognized as columns.
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
