b50ff82968791d8e887e57ef6fbb57abbb988274
kooprogramming <hsg9984@gmail.com>
2020-07-13 17:28:18 +0900

    20.07.13 구글로그인 설정 완료

---
+++ src/main/java/kr/co/kokono/webstudy/domain/UserRepository.java
@@ -0,0 +1,11 @@
+package kr.co.kokono.webstudy.domain;
+
+import kr.co.kokono.webstudy.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
--- src/main/java/kr/co/kokono/webstudy/web/IndexController.java
+++ src/main/java/kr/co/kokono/webstudy/web/IndexController.java
@@ -1,6 +1,7 @@
 package kr.co.kokono.webstudy.web;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
+import kr.co.kokono.webstudy.config.auth.LoginUser;
+import kr.co.kokono.webstudy.config.auth.dto.SessionUser;
 import kr.co.kokono.webstudy.service.posts.PostsService;
 import kr.co.kokono.webstudy.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
